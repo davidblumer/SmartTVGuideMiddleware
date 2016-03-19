ca5688d298e97f5cefdb40913fed14706b189849
Michael Malura <maluramichael@googlemail.com>
2016-03-19 11:40:02 +0100

    Add basic socket io server

---
+++ server.js
@@ -0,0 +1,12 @@
+const constants = require('./constants');
+
+var app = require('express')();
+var http = require('http').Server(app);
+
+app.get('/', (req, res)=> {
+	res.json({version: constants.VERSION, description: constants.DESCRIPTION});
+});
+
+http.listen(PORT, ()=> {
+	console.log(`listening on *:${constants.PORT}`);
+});
