0eb02f4dcadffc11adb5799694d24247a54fe9c4
Michael Malura <maluramichael@googlemail.com>
2016-03-19 16:15:08 +0100

    Comment code

--- scaffoldSocketio.js
+++ scaffoldSocketio.js
@@ -20,45 +20,55 @@ class Device {
 }
 
 module.exports = (io)=> {
+	// Connection middleware. Socket connects then save the type and udid in the socket object.
 	io.use((socket, next)=> {
 		const data = socket.request;
 		socket.type = data._query['type'];
 		socket.udid = data._query['udid'];
-
 		console.log(`new device ${socket.type} ${socket.udid}`);
 		next();
 	});
 
+	// Device connected, bind listeners and events.
 	io.on('connection', (socket)=> {
-		console.log('user connected');
+
+		// send connection event to backend. not really useful right now but who cares :D
 		api.userConnected();
 
+		// append the new device to the unbound list for later.
 		unboundDevices.push(new Device(socket));
 
+		// socket disconnects, remove it from the lists.
 		socket.on('disconnect', ()=> {
-			console.log('user disconnected');
-
-			_.remove(unboundDevices, (device)=>{
+			_.remove(unboundDevices, (device)=> {
 				return device.udid === socket.udid;
 			});
 
+			// send disconnect event to backend. not really useful right now but who cares :D
 			api.userDisconnected();
 		});
 
-		socket.on('switch_channel', (channel)=> {
-			console.log(`user switched the channel to ${channel}`);
-			api.userSwitchedChannel(channel)
-				.then((response)=> {
+		// just the tv events like switching the channel or requesting a qr code
+		if (socket.type === constants.TV) {
 
-				})
-				.catch((error)=> {
-					
-				});
-		});
+			// tv switched the channel. send event to backend to get meta data for the channel.
+			socket.on('switch_channel', (channel)=> {
 
-		socket.on('request_code', ()=> {
-			console.log(`user wants a code`);
-			socket.emit('receive_code', {code: socket.uuid});
-		});
+				// send channel switch to backend
+				api.userSwitchedChannel(channel)
+					.then((response)=> {
+						// on success send the meta data back to the tv and smartphone
+					})
+					.catch((error)=> {
+						// on error send the error message to the tv and smartphone
+					});
+			});
+
+			// tv is requesting a code. for now just send the udid that gets sent on connection
+			socket.on('request_code', ()=> {
+				console.log(`user wants a code`);
+				socket.emit('receive_code', {code: socket.udid});
+			});
+		}
 	});
 };
