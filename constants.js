560c3c5bf328de6f7ec5321b6e564cc4f78a1be9
Michael Malura <maluramichael@googlemail.com>
2016-03-19 15:45:42 +0100

    Add socket types. Add device class.

--- constants.js
+++ constants.js
@@ -2,5 +2,10 @@ module.exports = {
 	PORT: 55900,
 	VERSION: 1,
 	DESCRIPTION: '',
-	API: 'http://localhost:3000/api/'
+	API: 'http://localhost:3000/api/',
+
+	// Socket Types
+	UNKNOWN: 'unknown',
+	TV: 'tv',
+	PHONE: 'phone'
 };
--- scaffoldSocketio.js
+++ scaffoldSocketio.js
@@ -2,22 +2,48 @@
  * Created by helion on 19/03/16.
  */
 'use strict';
+const _ = require('lodash');
+
 const constants = require('./constants'),
 	api = require('./api');
 
+let boundDevices = new Map(); // devices that are bound. tv <> smartphone
+let unboundDevices = new Array(); // random unbound devices. tv | smartphone | smartphone | .... | tv
+
+class Device {
+	constructor(socket) {
+		this.qrCode = '';
+		this.type = socket.type;
+		this.socket = socket;
+	}
+}
+
 module.exports = (io)=> {
+	io.use((socket, next)=> {
+		const data = socket.request;
+		socket.type = data._query['type'];
+		next();
+	});
+
 	io.on('connection', (socket)=> {
 		console.log('user connected');
 		api.userConnected();
+
+		unboundDevices.push(new Device(socket));
 
 		socket.on('disconnect', ()=> {
 			console.log('user disconnected');
 			api.userDisconnected();
 		});
 
-		socket.on('switch-channel', (channel)=> {
+		socket.on('switch_channel', (channel)=> {
 			console.log(`user switched the channel to ${channel}`);
 			api.userSwitchedChannel();
+		});
+
+		socket.on('request_code', ()=> {
+			console.log(`user wants a code`);
+			socket.emit('receive_code', Math.random());
 		});
 	});
 };
